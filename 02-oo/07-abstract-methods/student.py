3f2fa0fa7c4b3c59adf3ee3452f474ed74f3fe5b
Abdulrahman-Al-Hasbani-UCLL <Abdulrahman.alhasbani@student.ucll.be>
2024-03-08 18:53:03 +0100

    Week 4 done

--- 02-oo/07-abstract-methods/student.py
+++ 02-oo/07-abstract-methods/student.py
@@ -1,1 +1,59 @@
 # Write your code here
+from abc import ABC, abstractmethod
+
+class A(ABC):
+    def a(self):
+        self.b()
+
+    def e(self):
+        self.c()
+
+    @abstractmethod
+    def b():
+        ...
+    @abstractmethod
+    def c():
+        ...
+
+class B(A):
+    def b(self):
+        self.a()
+
+    def c(self):
+        self.e()
+
+class C(B):
+    def f(self):
+        pass
+
+class F(ABC):
+    def a(self):
+        self.b()
+        self.f()
+    
+    @abstractmethod
+    def b():
+        ...
+    @abstractmethod
+    def f():
+        ...
+
+
+class D(A):
+    def b(self):
+        self.f()
+        
+    @abstractmethod
+    def f():
+        ...
+
+
+class E(D):
+    def c(self):
+        self.a()
+
+    def f(self):
+        self.e()
+
+    def g(self):
+        self.f()
