68b84dc4b7f946d582e9abd7b743d5943bc68a5a
Abdulrahman-Al-Hasbani-UCLL <Abdulrahman.alhasbani@student.ucll.be>
2024-06-12 21:34:37 +0200

    Agustus examen gedaan!

--- 10-exam-information/02-exam-2023/august-2023/01-transport/test-transport.py
+++ 10-exam-information/02-exam-2023/august-2023/01-transport/test-transport.py
@@ -1,11 +1,74 @@
-# Write your own tests for the housing.py file here.
+# Write your own tests for the transport.py file here.
 # You must include the tests asked for in the assignment for full credit.
 # You may add additional tests if you would like to test your code more thoroughly.
 # Additional tests will not result in a higher grade.
 # This file must be able to be run without error in order to receive credit for the required testing.
 ####
-# Schrijf hier je eigen tests voor het housing.py bestand.
+# Schrijf hier je eigen tests voor het transport.py bestand.
 # Je moet de gevraagde tests in de opdracht opnemen voor volledige waardering.
 # Je mag extra tests toevoegen als je je code grondiger wilt testen.
 # Extra tests zullen niet leiden tot een hoger cijfer.
 # Dit bestand moet zonder fouten uitgevoerd kunnen worden om punten te krijgen voor de vereiste testen.
+
+import pytest
+from transport import *
+@pytest.mark.parametrize(('nummerplaat','aantal_stoelen', 'lijst_passagiers', 'afstand'), [
+    ("ABC123", 4, [Passenger("1", "Alice pain", 100), Passenger("2", "Bob b", 15)], 5),
+    ("XYZ789", 2, [Passenger("3", "Charlie evens", 200), Passenger("4", "Dave d", 25)], 10),
+    ("DEF456", 3, [Passenger("5", "Eve t", 300), Passenger("6", "Frank f", 35)], 15)
+])
+def test_valid_taxi_max_occupants_succeeds(nummerplaat, aantal_stoelen, lijst_passagiers, afstand):
+    voorbeeld_taxi = Taxi(nummerplaat, aantal_stoelen)
+    
+    assert voorbeeld_taxi.is_available == True
+
+    voorbeeld_taxi.pickup(lijst_passagiers, afstand)
+
+    assert voorbeeld_taxi.amount_of_seats == aantal_stoelen
+    assert voorbeeld_taxi.number_of_occupants == len(lijst_passagiers)
+    assert voorbeeld_taxi.is_available == False
+    assert voorbeeld_taxi.license_plate == nummerplaat
+    #De belanrijke
+    assert voorbeeld_taxi.maximum_occupants == aantal_stoelen, f'De Taxi.maximum_occupants functie moet het zelfde zijn als het aantal stoelen.\n {voorbeeld_taxi.maximum_occupants} is niet het juiste resultaat, \n het juiste resultaat is: {aantal_stoelen}'
+
+@pytest.mark.parametrize(('nummerplaat','aantal_stoelen', 'lijst_passagiers', 'afstand'), [
+    ("ABC123", 3, [Passenger("1", "Alice pain", 100), Passenger("2", "Bob b", 15), Passenger("5", "Eve t", 300), Passenger("6", "Frank f", 35)], 5),
+    ("XYZ789", 1, [Passenger("3", "Charlie evens", 200), Passenger("4", "Dave d", 25)], 10),
+    ("DEF456", 0, [Passenger("5", "Eve t", 300)], 15)
+])
+def test_valid_taxi_max_occupants_fails(nummerplaat, aantal_stoelen, lijst_passagiers, afstand):
+    voorbeeld_taxi = Taxi(nummerplaat, aantal_stoelen)
+    
+    assert voorbeeld_taxi.is_available == True
+
+    with pytest.raises(ValueError):
+        voorbeeld_taxi.pickup(lijst_passagiers, afstand)
+
+
+@pytest.mark.parametrize(('license_plate','number_of_seats', 'passenger_list'), [
+    ("ABC123", 4, [Passenger("1", "Alice pain", 100), Passenger("2", "Bob b", 15)]),
+    ("XYZ789", 2, [Passenger("3", "Charlie evens", 200), Passenger("4", "Dave d", 25)]),
+    ("DEF456", 3, [Passenger("5", "Eve t", 300), Passenger("6", "Frank f", 35)])
+])
+def test_valid_bus_max_occupants_succeeds(license_plate, number_of_seats, passenger_list):
+    example_bus = Bus(license_plate, number_of_seats)
+    
+    for passenger in passenger_list:
+        example_bus.board(passenger)
+
+    assert example_bus.amount_of_seats == number_of_seats
+    assert example_bus.number_of_occupants == len(passenger_list)
+    assert example_bus.license_plate == license_plate
+    assert example_bus.maximum_occupants == number_of_seats * 2, f'The Bus.maximum_occupants function should be twice the number of seats.\n {example_bus.maximum_occupants} is not the correct result, \n the correct result is: {number_of_seats * 2}'
+
+@pytest.mark.parametrize(('license_plate','number_of_seats', 'passenger_list'), [
+    ("ABC123", 2, [Passenger("1", "Alice pain", 100), Passenger("2", "Bob b", 15), Passenger("5", "Eve t", 300), Passenger("6", "Frank f", 35), Passenger("9", "Eve t2", 300)]),
+    ("XYZ789", 1, [Passenger("3", "Charlie evens", 200), Passenger("4", "Dave d", 25), Passenger("9", "Eve t2", 300)]),
+    ("DEF456", 0, [Passenger("5", "Eve t", 300)])
+])
+def test_valid_bus_max_occupants_fails(license_plate, number_of_seats, passenger_list):
+    example_bus = Bus(license_plate, number_of_seats)
+    
+    with pytest.raises(RuntimeError):
+        for passenger in passenger_list:
+            example_bus.board(passenger)
--- 10-exam-information/02-exam-2023/august-2023/01-transport/transport.py
+++ 10-exam-information/02-exam-2023/august-2023/01-transport/transport.py
@@ -1,2 +1,112 @@
 # enter your code here to solve the transporation assignment
 # voer hier je code in om de vervoersvraag op te lossen
+from abc import ABC, abstractmethod
+
+class Passenger:
+    @staticmethod
+    def is_valid_name(name:str) -> bool:
+        if ' ' not in name:
+            return False
+        if len(name.split()) <2:
+            return False
+        return True
+    
+    def __init__(self, id:str, name:str, money:int) -> None:
+        self.id = id
+        if not self.is_valid_name(name):
+            raise ValueError
+        self.__name = name
+        self.money = money
+
+    @property
+    def name(self):
+        return self.__name
+    
+    @name.setter
+    def name(self, n_name:str):
+        if self.is_valid_name(n_name):
+            self.__name = n_name
+        else:
+            raise ValueError
+        
+class Vehicle(ABC):
+
+    def __init__(self, license_plate:str, amount_of_seats:int) -> None:
+        self.license_plate= license_plate
+        self.amount_of_seats = amount_of_seats
+        self.__occupants = dict()
+    
+    @property
+    def number_of_occupants(self):
+        return len(self.__occupants.keys())
+    
+    @property
+    @abstractmethod
+    def maximum_occupants(self):
+        ...
+    
+    def add_passenger(self, passenger: Passenger):
+        self.__occupants[passenger.id] = passenger
+    
+    def remove_passenger(self, passenger: Passenger):
+        del self.__occupants[passenger.id]
+    
+    def remove_all_passengers(self):
+        self.__occupants.clear()
+    
+    @property
+    def occupant_names(self):
+        names = list()
+        for key in self.__occupants.keys():
+            names.append(self.__occupants[key].name)
+        return names
+    
+class Taxi(Vehicle):
+    def __init__(self, license_plate: str, amount_of_seats: int) -> None:
+        super().__init__(license_plate, amount_of_seats)
+        self.is_available = True
+    
+    @property
+    def maximum_occupants(self):
+        return self.amount_of_seats
+    
+    def pickup(self, passengers:list[Passenger], distance):
+        if self.number_of_occupants == self.maximum_occupants or self.is_available == False or len(passengers)> self.maximum_occupants:
+            raise ValueError
+        
+        first_passenger = None
+        for _ in range(1):
+            first_passenger = passengers[0]
+        if first_passenger.money < (1+distance)+5:
+            raise RuntimeError
+        if 1+distance <5:
+            first_passenger.money -= 5
+        else:
+            first_passenger.money -= (1+distance)
+        passengers[0] = first_passenger
+
+        self.is_available = False
+        for item in passengers:
+            self.add_passenger(item)
+    
+    def dropoff(self):
+        self.remove_all_passengers()
+        self.is_available = True
+
+class Bus(Vehicle):
+    def __init__(self, license_plate: str, amount_of_seats: int) -> None:
+        super().__init__(license_plate, amount_of_seats)
+    
+    @property
+    def maximum_occupants(self):
+        return self.amount_of_seats*2
+     
+    def board(self, passenger:Passenger):
+        if self.maximum_occupants == self.number_of_occupants or passenger.money < 2 :
+            raise RuntimeError
+
+        passenger.money -= 2
+        self.add_passenger(passenger)
+    
+    def disembark(self, passenger:Passenger):
+        self.remove_passenger(passenger)
